4ebdd38af8b10eb28e7976f5210f993182c6dac9
Shea Bunge <info@bungeshea.com>
2013-05-25 13:51:34 +1000

    Removed shortcode_exists() function
    Updated DocBlocks

--- bookmarks-shortcode.php
+++ bookmarks-shortcode.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * Plugin Name: Bookmarks Shortcode
  * Plugin URI: http://bungeshea.com/plugins/bookmarks-shortcode/
  * Description: Creates [bookmarks], [links] and [blogroll] shortcodes that will generate an unordered list of your WordPress links. Preforms the same function as wp_list_bookmarks().
@@ -11,68 +11,58 @@
  * Version: 2.0
  */
 
-if ( ! function_exists( 'shortcode_exists' ) ) :
+/**
+ * @package   Bookmarks Shortcode
+ * @version   2.0
+ * @author    Shea Bunge <info@bungeshea.com>
+ * @copyright Copyright (c) 2011 - 2013, Shea Bunge
+ * @link      http://bungeshea.com/plugins/bookmarks-shortcode
+ * @license   http://opensource.org/licenses/MIT
+ */
 
-	/**
-	* Check if a shortcode is registered in WordPress.
-	*
-	* Examples: shortcode_exists( 'caption' ) - will return true.
-	* shortcode_exists( 'blah' ) - will return false.
-	*
-	* @param bool|string $shortcode The name of the shortcode to check
-	* @return bool Whether the shortcode exists or not
-	*/
-	function shortcode_exists( $shortcode = false ) {
-		global $shortcode_tags;
+/**
+ * Returns a formatted list of WordPress bookmarks.
+ * These are the ones that you manage through the
+ * 'Links' admin menu.
+ *
+ * @since  1.0
+ * @uses   wp_list_bookmarks() To print a formatted list of bookmarks
+ * @param  array $atts Attributes to be passed to the wp_list_bookmarks() function
+ * @return string The formatted list of bookmarks
+ */
+function bookmarks_shortcode( $atts ) {
+	ob_start();
+	wp_list_bookmarks( $atts );
+	$output = ob_get_contents();
+	ob_end_clean();
+	return $output;
+}
 
-		if ( ! $shortcode )
-			return false;
+/**
+ * Register the shortcodes if they don't already exist
+ *
+ * @uses $shortcode_tags To check if a shortcode has been already registered
+ */
+function register_bookmarks_shortcode() {
+	global $shortcode_tags;
 
-		if ( array_key_exists( $shortcode, $shortcode_tags ) )
-			return true;
+	$shortcodes = array(
+		'bookmarks',
+		'blogroll',
+		'links',
+	);
 
-		return false;
+	foreach ( $shortcodes as $shortcode ) {
+
+		if ( ! array_key_exists( $shortcode, $shortcode_tags ) ) {
+			add_shortcode( $shortcode, 'bookmarks_shortcode' );
+		}
 	}
 
-endif; // function exists check
+}
+add_action( 'init', 'register_bookmarks_shortcode' );
 
+/* Allow shortcodes to be used in sidebar text widgets */
 
-if( ! function_exists( 'bookmarks_shortcode' ) ) :
-
-	/**
-	 * Returns the list of WordPress
-	 * bookmarks.
-	 *
-	 * @uses wp_list_bookmarks() To print a formatted list of bookmarks
-	 * @param array $atts Attributes to be passed to the wp_list_bookmarks() function
-	 * @return string The formatted list of bookmarks
-	 */
-	function bookmarks_shortcode( $atts ) {
-		ob_start();
-		wp_list_bookmarks( $atts );
-		$output = ob_get_contents();
-		ob_end_clean();
-		return $output;
-	}
-
-	/* Register the [bookmarks] shortcode if it doesn't already exist */
-
-	if( ! shortcode_exists( 'bookmarks' ) )
-		add_shortcode( 'bookmarks', 'bookmarks_shortcode' );
-
-	/* Register the [blogroll] shortcode if it doesn't already exist */
-
-	if( ! shortcode_exists( 'blogroll' ) )
-		add_shortcode( 'blogroll', 'bookmarks_shortcode' );
-
-	/* Register the [links] shortcode if it doesn't already exist */
-
-	if( ! shortcode_exists( 'links' ) )
-		add_shortcode( 'links', 'bookmarks_shortcode' );
-
-	/* Allow shortcodes to be used in sidebar text widgets */
-
-	add_filter( 'widget_text', 'shortcode_unautop' );
-	add_filter( 'widget_text', 'do_shortcode' );
-
-endif; // function exists check
+add_filter( 'widget_text', 'shortcode_unautop' );
+add_filter( 'widget_text', 'do_shortcode' );
