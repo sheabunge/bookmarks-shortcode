sheabunge/bookmarks-shortcode

commit 89f88e2d51a6adf66d009cd0a2113da5dd225d0b

Remove argument parsing from shortcode callback

bookmarks-shortcode.php
@@ -26,28 +26,16 @@
  * These are the ones that you manage through the
  * 'Links' admin menu.
  *
- * @since  1.0
- * @uses   wp_list_bookmarks() To print a formatted list of bookmarks
- * @param  array $atts Attributes to be passed to the wp_list_bookmarks() function
+ * @since 1.0
+ * @uses wp_list_bookmarks() To print a formatted list of bookmarks
+ *
+ * @param array $atts Attributes to be passed to the wp_list_bookmarks() function
+ *
  * @return string The formatted list of bookmarks
  */
 function bookmarks_shortcode( $atts = array() ) {
-	$defaults = array(
-		'title_li'         => false,
-		'title_before'     => '<h2>',
-		'title_after'      => '</h2>',
-		'category_before'  => false,
-		'category_after'   => false,
-		'categorize'       => true,
-		'show_description' => true,
-		'between'          => '<br />',
-		'show_images'      => false,
-		'show_rating'      => false,
-		'echo'             => false,
-	);
-
-	$args = wp_parse_args( $atts, $defaults );
-	return wp_list_bookmarks( $args );
+	$atts['echo'] = false;
+	return wp_list_bookmarks( $atts );
 }
 
 /**

commit cb5987992093523262312263ad6e72fac724a9c6

Add type check to shortcode attributes (fix #1)
Regression from 89f88e2 in v2.3 breaks echo=false behavior if strings
are passed to the shortcode. This adds a minimal check for three possible
arguments without reverting to wp_parse_args().

bookmarks-shortcode.php
@@ -34,7 +34,13 @@
  * @return string The formatted list of bookmarks
  */
 function bookmarks_shortcode( $atts = array() ) {
-	$atts['echo'] = false;
+	if ( empty( $atts ) ) {
+		$atts = array( 'echo' => false );
+	} elseif ( is_array( $atts ) ) {
+		$atts['echo'] = false;
+	} elseif ( is_string( $atts ) ) {
+		$atts = 'echo=false&' . $atts;
+	}
 	return wp_list_bookmarks( $atts );
 }
 